c2151a8e50aac4c21bf715955ae77efa9b88e07c
shengrui <shengrui.qsh@gmail.com>
2026-09-09 22:27:23 +0800

    Add guarded Xianyu profile cleanup options

--- tests/test_xianyu.py
+++ tests/test_xianyu.py
@@ -35,14 +35,41 @@ class XianyuTests(unittest.TestCase):
 
     def test_unknown_structures(self):
         for value in [None, [], 3, 'text', {}, {'data': None}, {'data': []}, {'data': {'sections': {}, 'resultList': 'text'}}, {'data': {'sections': [None, {}, 2], 'feedsCount': 100}}]:
-            for index in range(2):
+            for index in range(4):
                 self.assertEqual(run(index, value), value)
+
+    def test_profile(self):
+        def section(name, **item):
+            return {'template': {'name': name}, 'item': item}
+        account = section('my_fy25_user_info', level={'banner': 'x'}, nickname='keep')
+        trade = section('trade', orders=4)
+        unknown = section('new_unknown')
+        value = {'data': {'container': {'sections': [account, trade, unknown, None, section('my_fy25_slider'), section('xianyu_home_fish_my_banner_card_2023'), section('my_fy25_community'), section('my_fy25_tools')]}}}
+        clean = run(2, value)
+        sections = clean['data']['container']['sections']
+        self.assertEqual(len(sections), 5)
+        self.assertEqual(sections[0]['item'], {'nickname': 'keep'})
+        self.assertEqual(sections[1:4], [trade, unknown, None])
+        self.assertEqual(len(run(3, clean)['data']['container']['sections']), 4)
+        self.assertEqual(len(run(3, value)['data']['container']['sections']), 7)
+        for bad in [None, [], 3, {}, {'sections': None}, {'sections': {}}, {'sections': [None, 3, {}, {'item': None}]}]:
+            obj = {'data': {'container': bad}}
+            self.assertEqual(run(2, obj), obj)
+            self.assertEqual(run(3, obj), obj)
+
+    def test_profile_urls(self):
+        for host in ['acs.m.goofish.com', 'g-acs.m.goofish.com']:
+            url = 'https://' + host + '/gw/mtop.idle.user.page.my.adapter/1.0/'
+            for pattern in PATTERNS[4:6]:
+                self.assertTrue(pattern.search(url))
+                self.assertFalse(pattern.search(url.replace('adapter/', 'adapter.extra/')))
+                self.assertFalse(pattern.search(url.replace(host, host + '.evil')))
 
     def test_url_boundaries(self):
         urls = ['https://acs.m.taobao.com/gw/mtop.taobao.idle.home.welcome/1.0/?x=1', 'https://acs.m.goofish.com/gw/mtop.taobao.idle.user.strategy.get/1.0/', 'https://g-acs.m.goofish.com/gw/mtop.taobao.idlehome.home.nextfresh/1.0/', 'https://g-acs.m.goofish.com/gw/mtop.taobao.idlemtopsearch.search/1.0/']
-        self.assertEqual(len(PATTERNS), 4)
+        self.assertEqual(len(PATTERNS), 7)
         for index, url in enumerate(urls):
-            self.assertEqual([bool(p.search(url)) for p in PATTERNS], [i == index for i in range(4)])
+            self.assertEqual([bool(p.search(url)) for p in PATTERNS[:4]], [i == index for i in range(4)])
             for bad in [url.replace('/1.0/', 'extra/1.0/'), url.replace('.com/', '.com.evil/'), url.replace('/gw/', '/h5/'), url.replace('https:', 'http:')]:
                 self.assertFalse(any(p.search(bad) for p in PATTERNS))
 
